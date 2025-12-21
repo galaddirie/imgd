315cfd4355ce65155af6fce4cff46da682682676
Galad Dirie <galad.work@gmail.com>
2025-12-20 23:08:02 -0500

    remove old migrations

--- priv/repo/migrations/20251211110400_add_pinned_outputs_to_workflows.exs
+++
@@ -1,9 +0,0 @@
-defmodule Imgd.Repo.Migrations.AddPinnedOutputsToWorkflows do
-  use Ecto.Migration
-
-  def change do
-    alter table(:workflows) do
-      add :pinned_outputs, :map, default: %{}
-    end
-  end
-end
--- priv/repo/migrations/20251218224350_allow_null_workflow_version_on_executions.exs
+++
@@ -1,9 +0,0 @@
-defmodule Imgd.Repo.Migrations.AllowNullWorkflowVersionOnExecutions do
-  use Ecto.Migration
-
-  def change do
-    alter table(:executions) do
-      modify :workflow_version_id, :binary_id, null: true, from: :binary_id
-    end
-  end
-end
