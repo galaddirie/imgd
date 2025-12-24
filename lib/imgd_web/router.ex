8396bbc4bab1a7f72d2609eb6357d8919420780e
Galad Dirie <galad.work@gmail.com>
2025-12-24 17:53:02 -0500

    Update router.ex

--- lib/imgd_web/router.ex
+++ lib/imgd_web/router.ex
@@ -59,7 +59,6 @@ defmodule ImgdWeb.Router do
       live "/users/settings", UserLive.Settings, :edit
       live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
       live "/vue_demo", VueDemoLive
-
     end
 
     post "/users/update-password", UserSessionController, :update_password
